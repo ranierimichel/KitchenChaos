1d6705df795b58015e2e5c8a038ce7d4cc600245
Ranieri <ranieri.michel@gmail.com>
2023-04-21 00:30:24 -0300

    Removing unnecessary imports

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class MusicManager : MonoBehaviour {
 
